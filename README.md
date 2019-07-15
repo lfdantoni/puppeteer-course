62d04320b7c53f945fd5c21785ff26ef09f1960d
Leonardo D'Antoni <ldantoni@solstice.com>
2019-07-15 09:54:53 -0300

    added a verify text test

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Puppeteer course practice
 
 https://www.udemy.com/advanced-node-for-developers/
+
+@types/jest -> to get jest intellisense in VSC
---
+++ src/index.test.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+
+jest.setTimeout(30000);
+
+test('we can launch a browser', async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: {
+      width: 1024,
+      height: 768
+    }
+  })
+
+  const page = await browser.newPage();
+  await page.goto('https://www.linguee.com/');
+
+  // const element = page.waitForSelector('div[class^=description-unit--skills-hub__lead-text]', {visible: true});
+  // console.log(element);
+
+  const text = await page.$eval('.wide_in_main div[class^=sectioncontent] .textleft h3', el => el.innerHTML);
+
+  expect(text).toEqual('A gateway to the world')
+  // await browser.close();
+})
+
+
+
+
+
