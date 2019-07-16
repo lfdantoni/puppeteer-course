82276bb81d21d3f04bf30c464154a693a104e768
Leonardo D'Antoni <ldantoni@solstice.com>
2019-07-16 10:19:38 -0300

    add beforeEach and afterEach event

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,12 @@
 const puppeteer = require('puppeteer');
 
+// increase the timeout because of the loading page
 jest.setTimeout(30000);
 
-test('we can launch a browser', async () => {
-  const browser = await puppeteer.launch({
+let page, browser;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
     headless: false,
     defaultViewport: {
       width: 1024,
@@ -11,13 +14,18 @@ test('we can launch a browser', async () => {
     }
   })
 
-  const page = await browser.newPage();
+  page = await browser.newPage();
   await page.goto('https://www.linguee.com/');
+})
 
+afterEach(async () => {
+  await browser.close();
+})
+
+test('we can launch a browser', async () => {
   const text = await page.$eval('.wide_in_main div[class^=sectioncontent] .textleft h3', el => el.innerHTML);
 
   expect(text).toEqual('A gateway to the world')
-  await browser.close();
 })
 
 
